309441f430092042b5878dcb239c7fa09ec0d96a
Greg Munker <gmunker@gmail.com>
2017-04-20 12:07:17 -0700

    upgraded to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -10,19 +10,16 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    ]
   }
 };
